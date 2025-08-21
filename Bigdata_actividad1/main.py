3f7526d6aa37ed4dd46ff750b3cad0d69af17ccd
McLogin123 <103536901+McLogin123@users.noreply.github.com>
2025-08-21 13:19:10 -0400

    Update main.py
    Cambio de ip "host" en uvicorn

--- Bigdata_actividad1/main.py
+++ Bigdata_actividad1/main.py
@@ -182,4 +182,5 @@ if __name__ == "__main__":
     for i in range (5):
         ph_sensor.add_lectura(ph_sensor.generar_valores())
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
